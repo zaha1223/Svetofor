1fab1387276455c805d608bc82437e3d2c679e3d
zaha1223 <muhammadzohidbahodirov04@gmail.com>
2022-10-18 19:37:00 +0500

    create traffic light

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const elRedBtn = document.querySelector("#red-btn");
+const elRedLight = document.getElementsByTagName("span")[0];
+const elYellowBtn = document.querySelector("#yellow-btn");
+const elYellowLight = document.getElementsByTagName("span")[1];
+const elGreenBtn = document.querySelector("#green-btn");
+const elGreenLight = document.getElementsByTagName("span")[2];
+
+const elDelbtn = document.querySelector(".del-btn");
+
+elRedBtn.addEventListener("click", () => {
+  elRedLight.classList.toggle("red");
+  elYellowLight.classList.remove("yellow");
+});
+elYellowBtn.addEventListener("click", () => {
+  elYellowLight.classList.toggle("yellow");
+  elRedLight.classList.remove("red");
+  elGreenLight.classList.remove("green");
+});
+elGreenBtn.addEventListener("click", () => {
+  elGreenLight.classList.toggle("green");
+  elYellowLight.classList.remove("yellow");
+  elRedLight.classList.remove("red");
+});
+elDelbtn.addEventListener("click", () => {
+  elGreenLight.classList.remove("green");
+  elRedLight.classList.remove("red");
+  elYellowLight.classList.remove("yellow");
+});
